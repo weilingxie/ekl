a975ed0c99e854cd9935f82c9abaf073c0751819
Weiling Xie <sqlnever@gmail.com>
2021-02-10 13:40:01 +1300

    Add Projects component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import Landing from './components/Landing'
 import AboutUs from './components/AboutUs'
+import Projects from './components/Projects'
 import ContactUs from './components/ContactUs'
 import './App.scss';
 
 function App() {
   return (    
     <Router>     
-      <Landing />      
+      <Landing />
       <AboutUs />
+      <Projects />      
       <ContactUs />      
     </Router>        
   );
