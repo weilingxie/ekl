e2fb114a421dc45b92840d9ebde8bb9fa13e3892
weilingxie1 <sqlnever@gmail.com>
2021-08-12 12:57:41 +1200

    Solved navbar - buttons toggle in mobilephone mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,32 @@
 import React from 'react'
 import Logo from '../images/EKL-logo.svg'
 
-
-//rfce + enter
-const Navbar = () => {
-    /*const [ click, setClick ] = useState(false);*/    
-
+const Navbar = () => {  
     return (        
           <div id="navbar" className="navbar"> 
-            <img className="logo" src={Logo} alt="EKL" />                                                                            
+            <img className="logo" src={Logo} alt="EKL Kitchens" />
+            <input type="checkbox" id="nav-toggle" className="nav-toggle"></input>                                                   
             <nav className="navbar-btns">
                 <ul>
                     <li className="navbar-btn navbar-home" >
-                    <a href="#home">HOME</a>
+                      <a href="#home">HOME</a>
                     </li>
                     <li className="navbar-btn navbar-aboutus">
-                    <a href="#aboutUs">ABOUT</a>
+                      <a href="#aboutUs">ABOUT</a>
                     </li>
                     <li className="navbar-btn navbar-services">
-                    <a href="#services">SERVICES</a>
+                      <a href="#services">SERVICES</a>
                     </li>
                     <li className="navbar-btn navbar-projects">
-                    <a href="#projects">PROJECTS</a>
+                      <a href="#projects">PROJECTS</a>
                     </li>
                     <li className="navbar-btn navbar-contactus">
-                    <a href="#contactUs">CONTACT</a>
+                      <a href="#contactUs">CONTACT</a>
                     </li>
                 </ul>                  
             </nav>        
             <label htmlFor="nav-toggle"><i className="fas fa-bars fa-2x nav-toggle-label" ></i></label>           
-          </div>  
+          </div>
     )
 }
 
