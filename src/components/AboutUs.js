395e6cc5d158116080a5e34f19c37037b3dced92
weilingxie1 <sqlnever@gmail.com>
2021-11-07 11:15:11 +1300

    Updated About Title color

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,7 +5,7 @@ const AboutUs = ()=>{
         <div id="aboutUs" className ="section aboutUs">            
             <div className = "aboutus-container">
                 <div className = "text-container">
-                    <h1 className="section-title">ABOUT US</h1>
+                    <h1 className="section-title about-title">ABOUT US</h1>
                     <p className = "aboutus-text">EKL Kitchens is the leading New Zealand kitchen and cabinet manufacturer. Our mission is to provide our customers with innovative designs of high quality and competitive value. </p>
                     <p className = "aboutus-text">EKL Kitchens is based in Auckland where we have our manufacturing operations as well as our design team. We can tailor every detail of your dream kitchen and manufacture it with the best raw materials.</p>
                     <p className = "aboutus-text">Our factory is equipped with cutting edge CNC routers (computer-controlled cutting and shaping) and modern edging equipment. It ensures an unequalled precision in the making of your Kitchen elements and gives us more flexibility to answer your every need.</p>
