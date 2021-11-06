0201e5b975878b07b09fa6d7cbc2ff0e32e8243d
weilingxie1 <sqlnever@gmail.com>
2021-11-07 12:17:53 +1300

    Replaced images

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const Services = () => (        
-    <div id="services" className ="section services">            
-        <div className = "services-container">                
-            <img className="image-container" src={`${process.env.PUBLIC_URL}/images/AboutusImage.jpg`} alt="Services" />                    
-            <div className = "text-container">
-                <div className = "text-wrapper">
-                    <h1 className="section-title">SERVICES</h1>
-                    <ul>
-                        <li className = "services-text">Kitchen cabinets customize</li>
-                        <li className = "services-text">Interal design</li>
-                        <li className = "services-text">3D Drawing</li>
-                        <li className = "services-text">Shop fitting and renovations</li>                       
-                    </ul>
-                    <p>Free quotes and on site measurement</p>
-                </div>
-            </div>                               
+const Services = () => (
+  <div id="services" className="section services">
+    <div className="services-container">
+      <img
+        className="image-container"
+        src={`${process.env.PUBLIC_URL}/images/ServicesImage.jpg`}
+        alt="Services"
+      />
+      <div className="text-container">
+        <div className="text-wrapper">
+          <h1 className="section-title">SERVICES</h1>
+          <ul>
+            <li className="services-text">Kitchen cabinets customize</li>
+            <li className="services-text">Interal design</li>
+            <li className="services-text">3D Drawing</li>
+            <li className="services-text">Shop fitting and renovations</li>
+          </ul>
+          <p>Free quotes and on site measurement</p>
         </div>
+      </div>
     </div>
-)
+  </div>
+);
 
-export default Services
+export default Services;
