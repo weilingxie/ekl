9b08f8f2ec2ec183769260f7d1fd18639930f2ec
weilingxie1 <sqlnever@gmail.com>
2021-11-18 21:38:44 +1300

    Modified map to use an image instead of Google API:

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import MapSection from './map/Map'
-import ContactInfo from './Footer/ContactInfo'
-import ContactForm from './Footer/ContactForm'
+import React from "react";
+import MapSection from "./map/Map";
+import ContactInfo from "./Footer/ContactInfo";
+import ContactForm from "./Footer/ContactForm";
 
-const ContactUs = () => (        
-    <div id="contactUs" className="section contactUs">
-        <div className="info-container">
-            <h1 className="section-title">CONTACT US</h1>
-            <ContactInfo />
-        </div>                       
-        <ContactForm />
-        <MapSection zoomLevel={17} />
-    </div>  
-)
+const BaseImageUri = `${process.env.PUBLIC_URL}/images/`;
 
-export default ContactUs
+const ContactUs = () => (
+  <div id="contactUs" className="section contactUs">
+    <div className="info-container">
+      <h1 className="section-title">CONTACT US</h1>
+      <ContactInfo />
+    </div>
+    <ContactForm />
+    <div className="map">
+      <img src={`${BaseImageUri}Map.png`} />
+    </div>
+  </div>
+);
+
+export default ContactUs;
